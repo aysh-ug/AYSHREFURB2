84c3347879c09068fdd15968ea9997eb6315c998
Ochola Alfred <59102788+aysh-ug@users.noreply.github.com>
2021-07-24 14:48:57 +0300

    Update gsheets.js

--- js/gsheets.js
+++ js/gsheets.js
@@ -1,31 +1,82 @@
-function init() {
-    Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vSJwXEKoxNt73ZVl2xMmKh_JmyJ-zIRNcsnVMLOVLClXToHQFQ02EeGMSApsTZiEXc_UcTJb5zdTBEh/pubhtml',
-    
-    callback: function(data, tabletop) {
-    console.log(data)
-    },
-    simpleSheet: true } )
-    }
-    window.addEventListener(‘DOMContentLoaded’, init)
 
-    function init() {
-        Tabletop.init( { key: ‘https://docs.google.com/spreadsheets/d/e/2PACX-1vSJwXEKoxNt73ZVl2xMmKh_JmyJ-zIRNcsnVMLOVLClXToHQFQ02EeGMSApsTZiEXc_UcTJb5zdTBEh/pubhtml',
-            callback: showInfo,
-        simpleSheet: true } )
-        }
-        function showInfo(data, tabletop) {
-        console.log(data);
-        for (var i = 0; i < data.length; i++) {
-        $(‘.post’).append(
-        ‘<div class=”article” style=”background-image: url( ‘ +
-        data[i].img + ‘.jpg)”>’ +
-        ‘<div class=”text”>’ +
-        ‘<h1>’ + data[i].title + ‘</h1>’+
-        data[i].body +
-        ‘</div> ‘ + ‘<div class=”meta-info”>’ + ‘<img src =”’ + data[i].img + ‘.jpg”>’+
-        ‘<div class=”date”>’ + data[i].date + ‘</div>’ +
-        ‘</div>’ +
-        ‘</div>’);
-        }
-        }
-        window.addEventListener(‘DOMContentLoaded’, init)
+  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1DpRZ4IA6Xd_rd4sNnkpuTkp_-6KOEJJWS-VqZxw4oU0/edit?usp=sharing';
+  
+  function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
+
+ 
+function showInfo(data, tabletop) {
+data.forEach(function(data) {
+    header.innerHTML = data.header;
+    header2.innerHTML = data.header2;
+    body.innerHTML = data.body;
+    body2.innerHTML = data.body2;
+    body3.innerHTML = data.body3;
+    body4.innerHTML = data.body4;
+    body5.innerHTML = data.body5;
+    body6.innerHTML = data.body6;
+    body7.innerHTML = data.body7;
+    body8.innerHTML = data.body8;
+    body9.innerHTML = data.body9;
+    body10.innerHTML = data.body10;
+    body11.innerHTML = data.body11;
+    body12.innerHTML = data.body12;
+    body13.innerHTML = data.body13;
+    body14.innerHTML = data.body14;
+    body15.innerHTML = data.body15;
+    body16.innerHTML = data.body16;
+    body17.innerHTML = data.body17;
+
+ });
+}
+  window.addEventListener('DOMContentLoaded', init)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
