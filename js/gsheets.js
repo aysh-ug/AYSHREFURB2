0edd6c9feb776a5283a4dd4b2f3f5a51b8e77de5
Ochola Alfred <59102788+aysh-ug@users.noreply.github.com>
2021-07-24 15:04:06 +0300

    Update gsheets.js

--- js/gsheets.js
+++ js/gsheets.js
@@ -1,5 +1,5 @@
 
-  var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1DpRZ4IA6Xd_rd4sNnkpuTkp_-6KOEJJWS-VqZxw4oU0/edit?usp=sharing';
+  var publicSpreadsheetUrl = '';
   
   function init() {
     Tabletop.init( { key: publicSpreadsheetUrl,
